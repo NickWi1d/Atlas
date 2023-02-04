ec99ea31ad6eb29b5d22e83078dd41e5e33b47ea
nickwi1d <nikyaeld@gmail.com>
2023-02-04 23:08:50 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,128 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div class="header">
+      <div class="logo">
+        <a class="jss42 jss43" href="/">
+          <svg width="194" height="39" viewBox="0 0 194 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M7.404.345l37.027 13.877a5.5 5.5 0 0 1 0 10.3L7.404 38.4a5.42 5.42 0 0 1-6.978-3.18 5.433 5.433 0 0 1 .111-4.082l4.185-9.559a5.504 5.504 0 0 0 0-4.414L.537 7.606a5.43 5.43 0 0 1 2.79-7.15A5.416 5.416 0 0 1 7.404.345z" fill="url(#a)"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.474 31.143a5.432 5.432 0 0 1 .11 4.08 5.418 5.418 0 0 1-5.028 3.523h-.158a5.42 5.42 0 0 1-5.054-3.523 5.433 5.433 0 0 1 .111-4.08l4.08-9.33c.568-1.297.66-2.925.277-4.328l3.755 9.037 1.907 4.621z" fill="url(#b)"></path>
+            <path d="M74.774 21.74a.212.212 0 0 0 .032.192c.043.064.096.096.16.096h5.315c.064 0 .118-.032.16-.096a.211.211 0 0 0 .032-.192l-2.817-10.048c0-.021-.01-.032-.032-.032s-.032.011-.032.032L74.774 21.74zM69.3 31.084c-.448 0-.8-.181-1.056-.544a1.236 1.236 0 0 1-.16-1.184L74.71 9.452c.192-.512.502-.928.929-1.248.448-.32.939-.48 1.473-.48h1.152c.555 0 1.046.16 1.473.48.448.32.758.736.928 1.248l6.628 19.904c.15.427.096.822-.16 1.184a1.224 1.224 0 0 1-1.057.544h-1.152c-.513 0-.993-.16-1.441-.48a2.504 2.504 0 0 1-.865-1.28l-1.024-3.616c-.021-.17-.139-.256-.352-.256h-7.236c-.192 0-.31.086-.352.256l-1.025 3.616c-.128.512-.416.939-.864 1.28-.427.32-.908.48-1.441.48h-1.025zM97.115 11.436c-.491 0-.918-.17-1.281-.512a1.75 1.75 0 0 1-.544-1.28v-.096c0-.49.181-.917.544-1.28.363-.362.79-.544 1.28-.544h13.64c.491 0 .918.182 1.281.544.363.363.544.79.544 1.28v.096c0 .491-.181.918-.544 1.28-.363.342-.79.512-1.281.512h-4.29c-.171 0-.256.096-.256.288V29.26c0 .491-.182.918-.545 1.28-.363.363-.789.544-1.28.544h-.897c-.491 0-.918-.181-1.281-.544-.362-.362-.544-.789-.544-1.28V11.724c0-.192-.085-.288-.256-.288h-4.29zM122.469 31.212a1.503 1.503 0 0 1-1.249-.384 1.556 1.556 0 0 1-.512-1.184c0-.49.16-.917.48-1.28a2.569 2.569 0 0 1 1.249-.736c1.131-.298 1.91-1.002 2.337-2.112.427-1.109.64-3.018.64-5.728V9.548c0-.49.171-.917.512-1.28.363-.362.79-.544 1.281-.544h12.135c.491 0 .907.182 1.248.544.363.363.545.79.545 1.28V29.26c0 .491-.182.918-.545 1.28a1.648 1.648 0 0 1-1.248.544h-.865a1.75 1.75 0 0 1-1.28-.544c-.363-.362-.545-.789-.545-1.28V11.628c0-.192-.085-.288-.256-.288h-6.243c-.171 0-.256.096-.256.288v8.096c0 4.032-.545 6.859-1.633 8.48-1.068 1.622-2.999 2.624-5.795 3.008zM157.036 21.74a.212.212 0 0 0 .032.192c.043.064.096.096.16.096h5.315c.064 0 .118-.032.16-.096a.209.209 0 0 0 .032-.192l-2.817-10.048c0-.021-.011-.032-.032-.032-.022 0-.032.011-.032.032l-2.818 10.048zm-5.475 9.344c-.448 0-.8-.181-1.056-.544a1.235 1.235 0 0 1-.161-1.184l6.628-19.904c.192-.512.502-.928.929-1.248.448-.32.939-.48 1.472-.48h1.153c.555 0 1.046.16 1.473.48.448.32.758.736.928 1.248l6.628 19.904c.149.427.096.822-.16 1.184a1.224 1.224 0 0 1-1.057.544h-1.152c-.513 0-.993-.16-1.441-.48a2.508 2.508 0 0 1-.865-1.28l-1.024-3.616c-.021-.17-.139-.256-.352-.256h-7.236c-.192 0-.31.086-.352.256l-1.025 3.616c-.128.512-.416.939-.864 1.28-.427.32-.908.48-1.441.48h-1.025zM187.893 31.404c-3.372 0-6.051-1.034-8.036-3.104-1.964-2.09-2.946-5.056-2.946-8.896 0-3.797.95-6.741 2.85-8.832 1.921-2.112 4.6-3.168 8.036-3.168 1.58 0 2.903.096 3.97.288.513.086.94.342 1.281.768.342.427.512.907.512 1.44v.256c0 .448-.192.8-.576 1.056-.363.256-.758.331-1.185.224-1.046-.277-2.22-.416-3.522-.416-2.113 0-3.778.726-4.994 2.176-1.196 1.451-1.793 3.52-1.793 6.208 0 2.667.619 4.736 1.857 6.208 1.259 1.451 2.935 2.176 5.027 2.176 1.408 0 2.614-.128 3.617-.384a1.385 1.385 0 0 1 1.185.224c.384.256.576.608.576 1.056v.224c0 .534-.17 1.014-.512 1.44-.32.406-.747.651-1.281.736-1.216.214-2.572.32-4.066.32z" fill="#020A0D" fill-opacity=".9"></path>
+            <defs>
+              <linearGradient id="a" x1="16.021" y1="31.497" x2="24.047" y2="6.643" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#0260FD"></stop>
+                <stop offset="1" stop-color="#0ACBFF"></stop>
+              </linearGradient>
+              <linearGradient id="b" x1="2.174" y1="37.568" x2="8.246" y2="30.815" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#24D0FF"></stop><stop offset="1" stop-color="#06B2FE"></stop>
+              </linearGradient>
+            </defs>
+          </svg>
+        </a>
+      </div>
+      <div class="login">
+        <button class="logInButton">
+          <span class="spanLogin">
+              <svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path>
+              </svg>
+              Войти
+          </span>
+        </button>
+      </div>
+    </div>
+    <div class="popular"></div>
+    <div class="search">
+      <div class="params">
+        <h1>Фильтр</h1>
+        <div class="from">
+          <p>Откуда</p>
+          <input type="text" placeholder="Любой" class="bigInput">
+        </div>
+      </div>
+      <div class="resaults"></div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
-<style>
+<style> 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+.main{
+  width: 100%;
+  height: 1000px;
+  border:1px solid
+}
+.header{
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: space-between;
+}
+.logo{
+  width: 30%;
+  height: 100%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+.login{
+  width: 20%;
+  height: 100%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+.spanLogin{
+  display: flex;
+  align-items:center;
+  font-size: 20px
+}
+.logInButton{
+  width: 40%;
+  background-color: white;
+  border:none;
+}
+.logInButton:hover{
+  background-color:#8c8d8d;
+}
+.MuiSvgIcon-root{
+  margin-right: 10%;
+}
+.search{
+  width: 100%;
+  height: 50%;
+  border: 1px solid;
+  display: flex;
+  flex-direction: row;
+}
+.params{
+  width: 30%;
+  border: 1px solid;
+}
+.resaults{
+  width: 70%;
+  border: 1px solid;
+}
+.popular{
+  width: 100%;
+  height: 30%;
+}
+.bigInput{
+  width: 80%;
+  height: 60%;
+}
+.from{
+  width: 100%;
+  height: 10%;
 }
 </style>
